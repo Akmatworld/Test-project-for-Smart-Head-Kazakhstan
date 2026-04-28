25f3ccf3c15375b16d84cf73183ffa97887ba866
Akmat <sirazhidinovakmatali@gmail.com>
2026-04-28 15:59:27 +0600

    First commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <section class="hero">
+        <div class="container">
+            <h1>Figma layout here</h1>
+        </div>
+    </section>
+
+@endsection
